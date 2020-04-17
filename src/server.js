b6aaa659f155e222a72ca142313245d9e4954da8
anisa07 <askany07@gmail.com>
2020-04-18 02:10:37 +0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
+
 require('dotenv').config();
+
 const router = require('./routes/router');
 
 const app = express();
+
+const whitelist = [];
+// 'https://localhost:5000'
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
 
 let db;
 
@@ -14,9 +29,8 @@ if (process.env.DB === 'mongo') {
 }
 
 app.use(express.json());
-app.use('/jdi/usage/statistic', router);
+app.use('/jdi/usage/statistic', cors(corsOptions), router);
 // app.use('/statistic', express.static(__dirname + '/public'));
-
 
 https.createServer({
     key: fs.readFileSync(path.join('.','/keys/', 'server.key')),
